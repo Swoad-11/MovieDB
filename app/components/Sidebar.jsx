e53e5dea5ce000a7d3ab01abb0b2991df1846115
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-04-06 19:06:24 +0600

    dark mode implemented

---
+++ app/components/Sidebar.jsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import trending from "@/public/trending.svg";
+import newRelease from "@/public/newRelease.svg";
+import commingSoon from "@/public/commingSoon.svg";
+import favourite from "@/public/favourite.svg";
+import watchLater from "@/public/watchLater.svg";
+
+export default function Sidebar() {
+  return (
+    <aside>
+      <ul className="space-y-2">
+        <li>
+          <a
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-primary text-black"
+            href="#"
+          >
+            <Image src={trending} width="24" height="24" alt="" />
+            <span>Trending</span>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image src={newRelease} width="24" height="24" alt="" />
+            <span>New Releases</span>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image src={commingSoon} width="24" height="24" alt="" />
+            <span>Coming Soon</span>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image src={favourite} width="24" height="24" alt="" />
+            <span>Favourites</span>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+            href="#"
+          >
+            <Image src={watchLater} width="24" height="24" alt="" />
+            <span>Watch Later</span>
+          </a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
---
+++ app/providers/ThemeProvider.jsx
@@ -0,0 +1,19 @@
+"use client";
+import { useState } from "react";
+import { ThemeContext } from "../context";
+
+const ThemeProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkTheme((prev) => !prev);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ isDarkTheme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
