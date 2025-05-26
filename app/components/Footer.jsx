8e4f9ac65ca4b5bcd0e29cb8e3b737c163ec2aa6
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2025-05-26 22:24:31 +0600

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="py-6 md:py-8 mt-8">
       <div className="container mx-auto">
         <p className="text-center text-sm text-black/30 dark:text-[#EEEEEE]/60">
-          Copyright {currentYear} | All rights reserved by Learn with Sumit
+          Copyright {currentYear} | All rights reserved by Swoad
         </p>
       </div>
     </footer>
